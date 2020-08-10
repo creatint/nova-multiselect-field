19a20be972f440b62282d1b197c20ccb38f77a3d
Creatint <creatint@163.com>
2020-08-10 15:44:32 +0800

    Add class name

--- resources/js/components/NovaMultiselectDetailFieldValue.vue
+++ resources/js/components/NovaMultiselectDetailFieldValue.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative rounded-lg bg-white shadow border border-60" v-if="values">
-    <div class="overflow-hidden rounded-b-lg rounded-t-lg">
-      <div class="border-b border-50 cursor-text font-mono text-sm py-2 px-4" v-for="(value, i) of values" :key="i">
+    <div class="multiselect_box overflow-hidden rounded-b-lg rounded-t-lg">
+      <div class="multiselect_item border-b border-50 cursor-text font-mono text-sm py-2 px-4" v-for="(value, i) of values" :key="i">
         {{ value }}
       </div>
     </div>
